01e4bdede25342c90be3e6143dc131dc40f22274
AliyanJamshaid <aliyanjamshaid789@gmail.com>
2025-02-21 21:21:43 +0500

    add/feature fetch events from tickmaster

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Import your route files
-const authRoutes = require('./authRoutes');
-const constantsRoutes = require('./constantsRoutes');
-const preferencesRoutes = require('./preferencesRoutes');
-
+const authRoutes = require("./authRoutes");
+const constantsRoutes = require("./constantsRoutes");
+const preferencesRoutes = require("./preferencesRoutes");
+const eventsRoutes = require("./eventsRoutes");
 // Use the route files
-router.use('/auth', authRoutes);
-router.use('/constants', constantsRoutes);
-router.use('/preferences', preferencesRoutes);
+router.use("/auth", authRoutes);
+router.use("/constants", constantsRoutes);
+router.use("/preferences", preferencesRoutes);
+router.use("/events", eventsRoutes);
 
 // Export the router
 module.exports = router;
